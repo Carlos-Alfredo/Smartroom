85989a6cf95c092458e3935b716f80faad691764
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2022-02-02 08:53:23 -0300

    Regra de negócios e calibragem implementada

--- lamp.py
+++ lamp.py
@@ -1,3 +1,5 @@
+import dbCommunication as db
+import datetime
 
 class Lamp:#Classe lâmpada
 	
@@ -8,6 +10,10 @@ class Lamp:#Classe lâmpada
 		
 	
 	def set_luminosity(self,luminosity_set):#Método para mudar a intensidade do led
+		if luminosity_set>100:
+			return self.set_luminosity(100)
+		elif self.luminosity_set==luminosity_set:
+			return 1
 		self.luminosity_set=luminosity_set
 		#
 		#
@@ -17,16 +23,39 @@ class Lamp:#Classe lâmpada
 ########################################################################################################
 
 def business_rule(lamp):#Regra de negócios para a lâmpada, será uma thread
-	calibration_routine(lamp)
+	k_luminosity=calibration_routine(lamp)
+	telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+	target=db.read_target(db.db_conn_string,db.database_name,db.container_target_name)
 	while 1:
-		if(self.luminosity_target==0 or self.presence_telemetry==0):#Caso particular luz off
-			self.set_luminosity(0)
+		if(target[2]==0 or target[3]==0):#Caso particular luz off
+			lamp.set_luminosity(0)
 		else:
-			#
-			#
-			#
-			#
+			delta=target[2]-telemetry[2]
+			if (delta>2 or delta<-2):#Fora da margem aceitável de erro
+				lamp.set_luminosity(lamp.luminosity_set+delta/k_luminosity)
+		old_telemetry_time=telemetry[4]
+		telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+		target=db.read_target(db.db_conn_string,db.database_name,db.container_target_name)
+		while old_telemetry_time>=telemetry[4]:
+			telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+			target=db.read_target(db.db_conn_string,db.database_name,db.container_target_name)
+			
 def calibration_routine(lamp):#Rotina de calibração da lâmpada, executada no início de business_rule
-	initial_time=self.luminosity_telemetry[1]
-	self.set_luminosity
+	
+	lamp.set_luminosity(0)
+	time=datetime.datetime.utcnow()
+	telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+	while telemetry[4]<time:
+		telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+	light_level_turn_off=telemetry[2]
+	
+	lamp.set_luminosity(100)
+	time=datetime.datetime.utcnow()
+	telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+	while telemetry[4]<time:
+		telemetry=db.read_telemetry(db.db_conn_string,db.database_name,db.container_telemetry_name)
+	light_level_turn_on=telemetry[2]
 
+	k_luminosity=(light_level_turn_on-light_level_turn_off)/100
+
+	return k_luminosity
