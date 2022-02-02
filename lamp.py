f778e8e3b4dc11b78cb03775b8ef677dd99adefe
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2022-02-02 08:01:25 -0300

    Ideia inicial de objeto

---
+++ lamp.py
@@ -0,0 +1,32 @@
+
+class Lamp:#Classe lâmpada
+	
+	def __init__(self,luminosity_set):
+		self.luminosity_set=luminosity_set
+		self.set_luminosity(luminosity_set)
+		
+		
+	
+	def set_luminosity(self,luminosity_set):#Método para mudar a intensidade do led
+		self.luminosity_set=luminosity_set
+		#
+		#
+		#
+		#
+
+########################################################################################################
+
+def business_rule(lamp):#Regra de negócios para a lâmpada, será uma thread
+	calibration_routine(lamp)
+	while 1:
+		if(self.luminosity_target==0 or self.presence_telemetry==0):#Caso particular luz off
+			self.set_luminosity(0)
+		else:
+			#
+			#
+			#
+			#
+def calibration_routine(lamp):#Rotina de calibração da lâmpada, executada no início de business_rule
+	initial_time=self.luminosity_telemetry[1]
+	self.set_luminosity
+
