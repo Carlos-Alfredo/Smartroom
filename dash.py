c6aa5004495266b6f684553620357634ca0a6df0
Ricardo Junior <ricardo.junior@muvz.com.br>
2022-02-03 17:24:24 -0300

    a

--- dash.py
+++ dash.py
@@ -1,27 +1,36 @@
 import streamlit as st
 import pandas as pd
-from read_last_file import *
-from dbCommunication import *
+import ast
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 from datetime import datetime, timedelta
+import threading
 pd.set_option('plotting.backend', 'pandas_bokeh')
 
-db_conn_string='mongodb://bancodedadosteste:zpqRKVboAwSlLv0ktQULEGPdTGdRd4O6mqUEfNi8NFq9ggJIBqDzhNUPYBLb8BsXIeUXPGcAaOGI6Ja8GDq9wg==@bancodedadosteste.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@bancodedadosteste@'
-database_name="bancodedadosiot"
-
-container_telemetry_name="telemetry"
-container_target_name="set_target"
-
+CONNECTION_STR = "Endpoint=sb://testeiotbroker.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Tig31UtDJ9LY+epBCrzehYnPtM3KvbLjxMU9SycGcKY="
+TOPIC_NAME_TELEMETRIA = "telemetria"
+SUBSCRIPTION_NAME = "Interface_do_usuario"
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
 def app():
-	out = read_last_file(db_conn_string,database_name,container_telemetry_name)
-	data = out['EnqueuedTimeUtc'] - timedelta(hours=3)
-	print(data)
-	data = data.strftime("%m/%d/%Y, %H:%M:%S")
+	message = {
+        "temperatura": '',
+        "umidade": '',
+        "luminosidade": '',
+        "presenca": '',
+        "tempo": '',
+    }
+	servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+	with servicebus_client as client:
+		receiver = servicebus_client.get_subscription_receiver(topic_name=TOPIC_NAME_TELEMETRIA, subscription_name=SUBSCRIPTION_NAME, max_wait_time=5)
+		with receiver:
+			for msg in receiver:
+				message=ast.literal_eval(str(msg))
+				receiver.complete_message(msg)
+	out = message
 	st.markdown("<center><h1>Smartroom 📟</h1><center>", unsafe_allow_html=True)
 	st.markdown('<center><h4>Apresentação do trabalho da cadeira INTERNET DAS COISAS</h4><center>', unsafe_allow_html=True)
-	out=read_last_file(db_conn_string,database_name,container_telemetry_name)
 	st.markdown('---')
 	st.markdown('<center><h4>Monitoramento - Sensores 📊</h4><center>', unsafe_allow_html=True)
-	st.markdown(f'<center><h5>Ultima Atualização: {data}</h5><center><br>', unsafe_allow_html=True)
+	#st.markdown(f'<center><h5>Ultima Atualização: {data}</h5><center><br>', unsafe_allow_html=True)
 	left_column, right_column = st.columns(2)
 	# You can use a column just like st.sidebar:
 	#COLOCAR AS UNIDADES DE MEDIDAS
@@ -39,8 +48,8 @@ def app():
 	st.sidebar.title('Atuadores')
 	led = st.sidebar.slider('Nivel de Luminosidade: ', 0, 100, 0)
 	temperatura = st.sidebar.slider('Temperatura: ', 0, 100, 0)
-	umidade = 0
-	presenca = 0
-	attribute_value = [temperatura, umidade, led, presenca]
-	send_target(db_conn_string,database_name,container_target_name,attribute_value)
+	
+	print(message)
+	
+
 app()
